e4a923805f1f48a4c1e6d3a9c56f08acc453cd83
Przemus14s <przemekszynal5@gmail.com>
2025-06-06 19:54:50 +0200

    Poprawki

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services") // już masz, ok
+    id("com.google.gms.google-services") 
 }
 
 android {
@@ -30,33 +30,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    // Dodaj jeśli używasz Kotlin (opcjonalnie)
-    // kotlinOptions {
-    //     jvmTarget = "11"
-    // }
 }
 
 dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.PhilJay:MPAndroidChart:3.1.0")
-
-    // Firebase BOM - zarządza wersjami Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
-
-    // Firebase Messaging
     implementation("com.google.firebase:firebase-messaging")
-
-    // Lifecycle process (do ProcessLifecycleOwner)
     implementation("androidx.lifecycle:lifecycle-process:2.6.1")
-
-    // AndroidX i inne biblioteki (zakładam, że libs.* są poprawnie zdefiniowane)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.mpandroidchart)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
